2a8b7c3a3d6c4be6dd33519e92c2ff9799b61fb0
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2021-11-30 14:34:00 -0500

    refactor date util, dynamic offeset

--- src/main/java/com/jm/demo/util/DateUtil.java
+++ src/main/java/com/jm/demo/util/DateUtil.java
@@ -1,21 +1,24 @@
 package com.jm.demo.util;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.util.Date;
 
 public class DateUtil {
 
+    private static final ZoneOffset offset = OffsetDateTime.now().getOffset();
+
     public static Date nowToDate() {
-        return Date.from(LocalDateTime.now().toInstant(ZoneOffset.ofHours(-6)));
+        return Date.from(LocalDateTime.now().toInstant(offset));
     }
 
     public static Date nowAfterHoursToDate(Long hours) {
-        return Date.from(LocalDateTime.now().plusHours(hours).toInstant(ZoneOffset.ofHours(-6)));
+        return Date.from(LocalDateTime.now().plusHours(hours).toInstant(offset));
     }
 
     public static Date nowAfterDaysToDate(Long days) {
-        return Date.from(LocalDateTime.now().plusDays(days).toInstant(ZoneOffset.ofHours(-6)));
+        return Date.from(LocalDateTime.now().plusDays(days).toInstant(offset));
     }
 
     private DateUtil() {
